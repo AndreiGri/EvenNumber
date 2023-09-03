d5e5f5d8098fffa9ffd52c996b4ab984feebfcd1
Andrei Gridin <andreigridin918@gmail.com>
2023-09-03 21:03:21 +0500

    Добавьте файлы проекта.

---
+++ EvenNumber/EvenNumber.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменяем кодировку консоли:
+    system("chcp 1251 > nul");
+
+    int a = 0;
+
+    cout << "Введите число: ";
+    cin >> a;
+
+    if (a % 2 == 0) {
+        cout << "Число " << a << " чётное!" << endl;
+    }
+    else {
+        cout << "Число " << a << " не чётное!" << endl;
+    }
+
+    // Задержка консольного окна:
+    system("pause > nul");
+    return 0;
+}
